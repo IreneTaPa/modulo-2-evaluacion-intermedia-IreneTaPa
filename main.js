a4c2ae5d50a2e3b9082b062c22c9d75e5d88cfbf
IreneTaPa <italavero@hotmail.com>
2023-05-31 09:13:37 +0200

    create const

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 'use strict'; 
 console.log('Holis'); 
 
+const selectNumber = document.querySelector('.js_select');
+const inputBet = document.querySelector('.js_input-bet');
+const playButton = document.querySelector('.js_input-btn');
+const message = document.querySelector('.js_message');
+const moneyBalance = document.querySelector('.js_balance');
+
